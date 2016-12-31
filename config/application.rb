1546606dd2e0b3f1ba740fc262525a1ff75d15fa
Doni Leung <doni@kudelabs.com>
2016-12-31 17:32:53 +0800

    Use haml instead erb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module DeployPortal
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths << Rails.root.join('lib')
+
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
