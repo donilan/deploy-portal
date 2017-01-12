fac4bb68518615e189b9f8ab14ee1fc8e962c194
Doni Leung <doni@kudelabs.com>
2017-01-12 15:53:04 +0800

    uniqueness for multi column for env

--- app/models/env.rb
+++ app/models/env.rb
@@ -1,5 +1,5 @@
 class Env < ApplicationRecord
   belongs_to :enviroment_group
   validates_format_of :key, with: /\A[a-z0-9\-_]+\Z/i
-  validates :key, uniqueness: true
+  validates :key, uniqueness: { scope: :env_group_id }
 end
